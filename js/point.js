ca41690b8c43d47a68928381c0dfc7a8c5cb67b6
wangyawei <wangyawei@douyu.tv>
2018-07-12 20:29:49 +0800

    Site updated: 2018-07-12 20:29:48

--- js/point.js
+++ js/point.js
@@ -1,9 +1,7 @@
-<script>
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?d4fd68f5e0e09ecac14118ba7ee211af";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-    })();
-</script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?d4fd68f5e0e09ecac14118ba7ee211af";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
